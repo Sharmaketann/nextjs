ae2020276f3552f69427932a76ddae6d43d7ba77
Sharmaketan <ketan.sharma@iensure.in>
2024-02-05 21:48:28 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="py-24">
+    <div className="container">
+      <h1 className='text-3xl font-bold container mt-4'>NextJS 13 Starter Kit</h1>
+    </div>
+    </section>
+  );
+}
